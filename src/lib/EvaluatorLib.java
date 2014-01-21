f41f9ba8f3180529f509b9dda3463ec8fd3b0906
ManaliThorat <manalit@thoughtworks.com>
2014-01-21 17:30:19 +0530

    addition and substraction is done

--- src/lib/EvaluatorLib.java
+++ src/lib/EvaluatorLib.java
@@ -1,15 +1,26 @@
 package lib;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class EvaluatorLib {
-    public int evaluate(String input) {
-        String[] data = input.split(" ");
-        for (String s : data) {
-            System.out.println(s);
-        }
-       return 1;
+    public String expression;
+    public EvaluatorLib(String expression) {
+        this.expression = expression;
+    }
+    public int evaluate(){
+        String[] resultArray = this.expression.split(" ");
+        int result = 0;
+        int num1 = Integer.parseInt(resultArray[0]);
+        int num2 = Integer.parseInt(resultArray[2]);
+        if(0 == resultArray[1].compareTo("+"))
+            result = addition(num1, num2);
+        if(0 == resultArray[1].compareTo("-"))
+            result = substraction(num1, num2);
+        return result;
+    }
+    public int addition(int num1,int num2){
+        return num1 + num2;
+    }
+    public int substraction(int num1,int num2){
+        return num1 - num2;
     }
 }
--- test/lib/EvaluatorLibTest.java
+++ test/lib/EvaluatorLibTest.java
@@ -11,8 +11,22 @@ public class EvaluatorLibTest {
     @Test
     public void testEvaluate() throws Exception {
         String input = "1 + 2";
-        EvaluatorLib evl = new EvaluatorLib();
-        int res = evl.evaluate(input);
-        assertEquals(res,1);
+        EvaluatorLib evl = new EvaluatorLib(input);
+        int res = evl.evaluate();
+        assertEquals(res,3);
+    }
+    @Test
+    public void testForAdditionOfTwoNumbers() throws Exception {
+        EvaluatorLib evl = new EvaluatorLib("2 + 3");
+        int expected = 5;
+        int actual = evl.evaluate();
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void testForSubsrtactionOfTwoNumbers() throws Exception {
+        EvaluatorLib evl = new EvaluatorLib("5 - 2");
+        int expected = 3;
+        int actual = evl.evaluate();
+        assertEquals(expected,actual);
     }
 }
