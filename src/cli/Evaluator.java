652c6794c1f001c020c52296b4102713b4abef24
ManaliThorat <manalit@thoughtworks.com>
2014-01-22 18:54:44 +0530

    updated

--- src/cli/Evaluator.java
+++ src/cli/Evaluator.java
@@ -5,7 +5,6 @@ import lib.EvaluatorLib;
 public class Evaluator {
     public static void main(String[] args) throws Exception {
         EvaluatorLib evl = new EvaluatorLib();
-        System.out.println(args[0]);
         int res = evl.EvaluateExpression(args[0]);
         System.out.println(res);
     }
