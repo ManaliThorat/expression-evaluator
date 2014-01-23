812d951f57ca11a2e902318aa7f2f20fe7a9f673
ManaliThorat <manalit@thoughtworks.com>
2014-01-23 13:05:02 +0530

    version 3.0 done

--- test/lib/EvaluatorLibTest.java
+++ test/lib/EvaluatorLibTest.java
@@ -12,42 +12,42 @@ public class EvaluatorLibTest {
     public void testEvaluate() throws Exception {
         String input = "1 + 2";
         EvaluatorLib evl = new EvaluatorLib();
-        int res = evl.EvaluateExpression(input);
+        int res = evl.evaluateExpression(input);
         assertEquals(res,3);
     }
     @Test
     public void testEvaluateForMultipleOperations() throws Exception {
         String input = "1 + 2 * 2";
         EvaluatorLib evl = new EvaluatorLib();
-        int res = evl.EvaluateExpression(input);
+        int res = evl.evaluateExpression(input);
         assertEquals(res,6);
     }
     @Test
     public void testEvaluateFor4Operations() throws Exception {
         String input = "1 + 2 * 2 - 2 / 2";
         EvaluatorLib evl = new EvaluatorLib();
-        int res = evl.EvaluateExpression(input);
+        int res = evl.evaluateExpression(input);
         assertEquals(res,2);
     }
     @Test
     public void testEvaluateForParanthisesOperations() throws Exception {
         String input = "(1 + 2) * 2";
         EvaluatorLib evl = new EvaluatorLib();
-        int res = evl.EvaluateExpression(input);
+        int res = evl.evaluateExpression(input);
         assertEquals(res,6);
     }
     @Test
     public void testEvaluateForParanthisesOperationsAtEnd() throws Exception {
         String input = "1 + (2 * 2)";
         EvaluatorLib evl = new EvaluatorLib();
-        int res = evl.EvaluateExpression(input);
+        int res = evl.evaluateExpression(input);
         assertEquals(res,5);
     }
     @Test
     public void testOnlyOneOperand() throws Exception {
         String input = "2";
         EvaluatorLib evl = new EvaluatorLib();
-        int res = evl.EvaluateExpression(input);
+        int res = evl.evaluateExpression(input);
         assertEquals(res,2);
     }
 
